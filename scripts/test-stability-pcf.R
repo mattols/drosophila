667e8c3de2a8df57b6e6b9958f39b5322cf58dc3
Matt Olson <mattolson@COS-23765-ESCI.local>
2025-08-24 13:11:12 -0600

    updated spatial assessments

--- scripts/test-stability-pcf.R
+++ scripts/test-stability-pcf.R
@@ -34,3 +34,33 @@ plot(pcf_cross$r, pcf_cross$numerator, type = "h",
 
 
 
+
+
+
+# # # # #
+
+# PCF within 6.5 km
+barplot(df_cross[df_cross$r<10 & df_cross$r>0,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
+# within next
+# # # # #
+
+# PCF within 15 km
+barplot(df_cross[df_cross$r<13 & df_cross$r>7,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
+
+# within next HUGE! (700,000 max)
+barplot(df_cross[df_cross$r<20 & df_cross$r>13,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
+
+# within next (370,000 max)
+barplot(df_cross[df_cross$r<26 & df_cross$r>20,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
+
+# 100,000 max
+barplot(df_cross[df_cross$r<34 & df_cross$r>26,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
+
+# 1,200 max
+barplot(df_cross[df_cross$r<40 & df_cross$r>34,'iso'], 
+        names.arg=df_cross[df_cross$r<10 & df_cross$r>0,'univ_rank'])
